c58dcfd89f4c8c2340d3c278873ca214c1cf18fa
dyameres <dyameres@davidson.edu>
2023-02-16 09:15:32 -0500

    Update symbolic_regresson.py

--- symbolic_regresson.py
+++ symbolic_regresson.py
@@ -1,3 +1,18 @@
 #
 #
 # By Dylan Ameres and Tristan Allen
+
+class Node():
+    def __init__(self, parent, value, left=None, right=None): #left and right are children
+        self.parent = parent
+        self.left = left
+        self.right = right
+        self.value = value
+
+
+class Tree():
+    def __init__(self,):
+
+    
+    def evaluate(x):
+        return x
