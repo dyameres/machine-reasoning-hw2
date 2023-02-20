a576b376ea83f7847c419ebd0956bf51bc175257
trallen <trallen@davidson.edu@gpu0.ada.davidson.edu>
2023-02-20 14:02:20 -0500

    adding some functions to get started

---
+++ .ipynb_checkpoints/symbolic_regresson-checkpoint.py
@@ -0,0 +1,19 @@
+#
+#
+# By Dylan Ameres and Tristan Allen
+
+class Node():
+    def __init__(self, parent, value, left=None, right=None): #left and right are children
+        self.parent = parent
+        self.left = left
+        self.right = right
+        self.value = value
+
+
+class Tree():
+    def __init__(self,):
+        #comment
+        
+    
+    def evaluate(x): # recursive
+        return x
--- symbolic_regresson.py
+++ symbolic_regresson.py
@@ -4,7 +4,6 @@
 
 class Node():
     def __init__(self, parent, value, left=None, right=None): #left and right are children
-        self.parent = parent
         self.left = left
         self.right = right
         self.value = value
@@ -12,8 +11,23 @@ class Node():
 
 class Tree():
     def __init__(self,):
-        #comment
+        # comment
+        # define self.root = root
+        # if statements for operators 
+        # else must be integers 
         
+    def getRoot(self):
+        # return self.root
+        
+    def fitness(self,):
+        # determine fitness of current tree
+        # maybe mean square error
     
     def evaluate(x): # recursive
         return x
+    
+    def crossover(self, otherTree):
+        # function for crossover
+        
+    def mutate(self,):
+        # function for mutation
